f00d10b8185dc1919dcf30934dfa38804c2c7dcf
Jacob Merizian <jmerizia@vt.edu>
2017-05-30 22:54:41 -0400

    fixed unload

--- unload.sh
+++ unload.sh
@@ -1,14 +1,14 @@
 echo 'Copying ~/.vim/'
-cp -rf .vim ~/.vim 
+cp -rf .vim ~
 
 echo 'Copying ~/.vimrc'
-cp .vimrc ~/.vimrc 
+cp .vimrc ~
 
 echo 'Copying ~/.tmux.conf.local'
-cp .tmux.conf.local ~/.tmux.conf.local 
+cp .tmux.conf.local ~
 
 echo 'Copying ~/.tmux/'
-cp -rf .tmux ~/.tmux 
+cp -rf .tmux ~
 
 echo 'Linking .tmux/.tmux.conf to .tmux.conf'
 ln ~/.tmux/.tmux.conf ~/.tmux.conf
